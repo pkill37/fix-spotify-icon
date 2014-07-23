5aa61681305d1137880c94c7c4c14894e1e066a1
faviouz <fabiu.maia@gmail.com>
2014-07-23 17:02:48 +0100

    Improve script

--- fix-spotify-icon.sh
+++ fix-spotify-icon.sh
@@ -1,20 +1,34 @@
+# Variables
+current_dir=$(pwd)
+tmp_dir="/tmp/fsi"
+
+# Do stuff in a temporary directory
+mkdir $tmp_dir
+cd $tmp_dir
+
+# Download icon
+wget -O spotify_icon.ico https://raw.githubusercontent.com/faviouz/fix-spotify-icon/master/spotify_icon.ico
+
 # Make a temporary copy of resources.zip
-cp /opt/spotify/spotify-client/Data/resources.zip tmp.zip
+cp /opt/spotify/spotify-client/Data/resources.zip resources_old.zip
 
 # Extract it so we replace the icon
-unzip tmp.zip -d tmp/
+unzip resources_old.zip -d resources_old/
 
 # Replace the icon with a new one
-cp spotify_icon.ico tmp/_linux/spotify_icon.ico
+cp spotify_icon.ico resources_old/_linux/spotify_icon.ico
 
 # Zip it back up
-cd tmp/
+cd resources_old/
 zip -r resources_patched.zip .
 cd ..
-mv tmp/resources_patched.zip .
+mv resources_old/resources_patched.zip .
 
-# Replace existing resources.zip with patched version
-mv resources_patched.zip /opt/spotify/spotify-client/Data/resources.zip
+# Replace existing resources.zip with the patched version
+cp resources_patched.zip /opt/spotify/spotify-client/Data/resources.zip
 
 # Clean up
-rm -rf tmp/ tmp.zip resources_patched.zip
+rm -rf $tmp_dir
+
+# Return to previous directory
+cd $current_dir
