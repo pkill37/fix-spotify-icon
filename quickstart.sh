372ec9f748105284d06b0bafee3a0e52702a3f06
faviouz <fabiu.maia@gmail.com>
2014-08-13 21:28:06 +0100

    add quickstart script

---
+++ quickstart.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+set -e
+
+wget -O fix-spotify-icon.sh https://raw.githubusercontent.com/faviouz/fix-spotify-icon/master/fix-spotify-icon.sh
+
+chmod +x fix-spotify-icon.sh
+
+sudo mv fix-spotify-icon.sh /usr/local/bin/fix-spotify-icon
+
+fix-spotify-icon
