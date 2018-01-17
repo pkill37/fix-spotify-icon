ec49faa32b07e58b06e4eb7699d3ff92af95b6f9
Fábio Maia <fabiu.maia@gmail.com>
2018-01-17 22:02:30 +0000

    Delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-set -e
-
-wget -O fix-spotify-icon.sh https://raw.githubusercontent.com/faviouz/fix-spotify-icon/master/src/fix-spotify-icon.sh
-
-chmod +x fix-spotify-icon.sh
-
-sudo mv fix-spotify-icon.sh /usr/local/bin/fix-spotify-icon
